4481604f81fd3209345e7d9023b3da747fa5a437
Manohari21 <52821947+Manohari21@users.noreply.github.com>
2020-09-17 11:18:15 +0530

    Home navigation changes

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+const { SECRET } = require('./helpers/constants');
 
 var loginRouter = require('./routes/login');
+var homeRouter = require('./routes/home');
 var eligibilityRouter = require("./routes/eligibility");
 var enrollmentRouter = require("./routes/enrollment");
 var app = express();
@@ -13,13 +17,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
 
 app.use('/', loginRouter);
+app.use('/home', homeRouter);
 app.use("/eligibility", eligibilityRouter);
 app.use("/enrollment", enrollmentRouter);
 
--- routes/login.js
+++ routes/login.js
@@ -4,17 +4,17 @@ var axios = require("axios");
 const { LOGIN_SERVICE_URL } = require("../helpers/constants");
 const logger = require("../helpers/logger");
 
+
 router.get('/', (req, res) => {
     logger.info("Login Page requested");
     res.render('login.ejs');
 });
 
-
 router.post('/validate', (req, res) => {
     logger.info("Validate page requested");
     var errormessage = 'Invalid Username/password';
-    let url = LOGIN_SERVICE_URL+`?name=${req.body.username}`;
-    logger.info(url);
+    let url = LOGIN_SERVICE_URL + `?name=${req.body.username}`;
+
     axios.get(url).then(function (response) {
         logger.info("Fetching user details from: " + url);
         const status = response.status;
@@ -24,14 +24,11 @@ router.post('/validate', (req, res) => {
             throw Error("Unable to continue, status received: " + status);
         }
         const data = response.data;
+        req.session.username = data.name;
+        req.session.userid = data.id;
         if (data.name == req.body.username && data.password == req.body.password) {
             logger.info("Successfully authenticated username: " + data.name);
-            //logger.info("role " + data.role)
-            // if (data.role == "user") {
-                res.render('homepage.ejs', { username: req.body.username,userid: data.id});
-            // } else if (data.role == "admin") {
-            //     res.render('admin_homepage.ejs', { username: req.body.username,userid: data.id });
-            // }
+            res.redirect('/home');
         }
         else {
             logger.info("Unable to log in username: " + data.name);
@@ -39,6 +36,7 @@ router.post('/validate', (req, res) => {
         }
     }).catch(function (error) {
         logger.error(`Error: ${error.message}`);
+
     });
 });
 
